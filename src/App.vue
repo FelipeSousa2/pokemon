4ee14c8e9c78f948432da1b6c4fc8182bd2eab3f
FelipeSousa@ <103294274+FelipeSousa2@users.noreply.github.com>
2022-08-11 18:38:47 -0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+    <pokeVue :pokemons="pokemons"/>
+</template>
+
+<script>
+import pokeVue from './components/poke.vue';
+
+export default {
+  name: "App",
+  components: {
+    pokeVue
+  },
+  
+  data() {
+    return{
+      pokemons:[]
+    }
+  }
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/poke.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="container">
+        <h1>Ola</h1>
+        {{pokemons}}
+    </div>
+</template>
+
+<script>
+export default {
+    name: "poke",
+    props: {
+        pokemons:[]
+    }
+}
+</script>
+
+<style>
+
+</style>
